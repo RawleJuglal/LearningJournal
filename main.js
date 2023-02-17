5fa1f068920231b0290478de079cacfb603fb673
Rawle Juglal <RawleJuglal@users.noreply.github.com>
2023-02-17 09:44:28 -0600

    html completed
    HTML for all pages completed but will need added utility styles

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 import './style.css'
+
+
+function toggleHidden(elem){
+  elem.classList.toggle('hidden')
+}
+
+document.addEventListener('click', (e)=>{
+  if(e.target.dataset.bars){
+    toggleHidden(document.getElementById(e.target.dataset.bars))
+  }
+})
