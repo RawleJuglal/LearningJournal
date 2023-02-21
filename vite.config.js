9a1446c4554750c35b5886416e9f6a6068248ab8
Rawle Juglal <RawleJuglal@users.noreply.github.com>
2023-02-20 23:05:12 -0600

    Update vite.config.js
    multipage vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build:{
+    rollupOptions:{
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        post: resolve(__dirname, 'post.html')
+      },
+    }
+  },
   base:'/LearningJournal/',
   plugins: [],
 })
